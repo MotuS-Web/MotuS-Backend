77a185eb85fd28bedd9e787f7d8fcf2f7502b97f
sirius506775 <sirius506775@gmail.com>
2023-10-07 03:28:23 +0900

    refactor : modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,24 @@
-#!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/motus/build/*.jar)
-JAR_NAME=$(basename "$BUILD_JAR")
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/motus/deploy.log
+#!/usr/bin/env bash
 
-echo "> build 파일 복사" >> /home/ubuntu/motus/deploy.log
-DEPLOY_PATH=/home/ubuntu/
-# shellcheck disable=SC2086
-cp $BUILD_JAR $DEPLOY_PATH
+REPOSITORY=/home/ubuntu/motus
+# shellcheck disable=SC2164
+cd $REPOSITORY
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/motus/deploy.log
-CURRENT_PID=$(pgrep -f "$JAR_NAME")
+APP_NAME=cicdproject
+# shellcheck disable=SC2010
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z "$CURRENT_PID" ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/motus/deploy.log
+  echo "> 종료할것 없음."
 else
-  echo "> kill -15 $CURRENT_PID"
-  # shellcheck disable=SC2086
-  kill -15 $CURRENT_PID
+  echo "> kill -9 $CURRENT_PID"
+  kill -15 "$CURRENT_PID"
   sleep 5
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/deploy.log
-nohup java -jar "$DEPLOY_JAR" >> /home/ubuntu/motus/deploy.log 2>/home/ubuntu/motus/deploy_err.log &
+echo "> $JAR_PATH 배포"
+nohup java -jar "$JAR_PATH" > /dev/null 2> /dev/null < /dev/null &
