5d14ab938ebb165aa9bbaa19a4109e9f6adcdd48
sirius506775 <sirius506775@gmail.com>
2023-10-07 00:45:03 +0900

    debug : add TestController

--- src/main/java/website/motus/TestController.java
+++ src/main/java/website/motus/TestController.java
@@ -1,5 +1,6 @@
 package website.motus;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
-    @RequestMapping("/ci-cd")
+    @GetMapping("/ci-cd")
     public String cicdTest(){
         return "CI / CD Test Success";
     }
