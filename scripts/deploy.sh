b354b2a70693be986ffb97b1b6c5f22399076b1a
sirius506775 <sirius506775@gmail.com>
2023-10-09 00:22:42 +0900

    build : modify deploy.sh (#30)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,21 +2,33 @@
 chmod +x /home/ubuntu/motus/scripts/deploy.sh
 
 REPOSITORY=/home/ubuntu/motus
+# shellcheck disable=SC2034
+APP_NAME=motus_CICD
+# shellcheck disable=SC2034
+APP_LOG="$PROJECT_ROOT/application.log"
+# shellcheck disable=SC2034
+ERROR_LOG="$PROJECT_ROOT/error.log"
+# shellcheck disable=SC2034
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+
 # shellcheck disable=SC2164
 cd $REPOSITORY
 
-APP_NAME=motus_CICD
 # shellcheck disable=SC2010
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep -f "$JAR_NAME")
 
 if [ -z "$CURRENT_PID" ]
 then
-  echo "> 종료할것 없음."
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> "$DEPLOY_LOG"
 else
-  echo "> kill -9 $CURRENT_PID"
+   # shellcheck disable=SC2086
+   echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
   kill -15 "$CURRENT_PID"
   sleep 5
 fi
